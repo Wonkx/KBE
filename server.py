6c8205313cd8d236b144edcc1403d3fead6c277f
Wonkx <williamkaldheim@gmail.com>
2023-03-06 12:02:22 +0100

    Add basic backend

---
+++ server.py
@@ -0,0 +1,37 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urls import route
+
+HOST_NAME = '127.0.0.1' 
+PORT_NUMBER = 1234 # Maybe set this to 1234
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+
+    def do_GET(s):        
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+
+        html = route(s.path)
+        s.wfile.write(bytes(html))
+
+    def do_POST(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        
+        html = route(s.path)
+        s.wfile.write(bytes(html))
+ 
+if __name__ == '__main__':
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), RequestHandler)
+    
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
