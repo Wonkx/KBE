6bf37fe8305131f3322458b08661c1acc16e89f1
wonkx <williamkaldheim@gmail.com>
2023-03-20 14:15:35 +0100

    #8 Add room basic model

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass
 
 @dataclass
+class Room:
+    roomLength: int = 10
+    roomWidth: int = 10
+    wallThickness: float = 0.3
+    roomOrigin: str = "point(0,0,0)"
+
+    def to_knowledge_fusion(self) -> str:
+        pass
+
+@dataclass
 class Building:
     stories: int = 10
     buildingOrigen: str = "point(0,0,0)"
