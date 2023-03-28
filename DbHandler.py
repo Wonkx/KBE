af26f5c7045d8d1d61cbcc0a52a6e30e847a0f38
wonkx <williamkaldheim@gmail.com>
2023-03-28 14:39:16 +0200

    Add test connection method to dbHandler

--- DbHandler.py
+++ DbHandler.py
@@ -1,4 +1,4 @@
-import requests 
+import requests
 
 HOST_NAME = '127.0.0.1' 
 PORT_NUMBER = 3030
@@ -6,3 +6,24 @@ DATASET = "kbe"
 
 def get_query_url():
     return "http://" + HOST_NAME + ":" + str(PORT_NUMBER) + "/" + DATASET + "/query"
+
+def test_connection() -> bool:
+    test_query = """
+                SELECT ?subject ?predicate ?object
+                WHERE {
+                    ?subject ?predicate ?object
+                }
+                LIMIT 1
+                """
+    
+    PARAMS = {'query': test_query} 
+    
+    try:
+        r = requests.post(url = get_query_url(), data = PARAMS)
+        return True
+    except:
+        return False
+
+
+if __name__ == '__main__':
+    print(test_connection())
